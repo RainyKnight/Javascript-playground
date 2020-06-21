67d596a48345a7a7c02810d5e2aa793d94bfdf3d
RainyKnight <stoltmanlucas@gmail.com>
2020-06-20 23:29:11 -0700

    Predicted thingAMaJig() console output

--- Script.js
+++ Script.js
@@ -1,10 +1,11 @@
 function thingAMaJig() {
 
+    // print "clunk" for every time that's passed in
     function clunk(times) {
         var num = times;
         while (num > 0) {
             display("clunk");
-            num = num - 1;
+            num--;
         }
     }
     
@@ -12,13 +13,15 @@ function thingAMaJig() {
         var facky = 1;
         clunkCounter = 0;
         if (size == 0) {
+            // calls the display function to post in console and increase clunkcounter
             display("clank");
         } else if (size == 1) {
             display("thunk");
         } else {
             while (size > 1) {
+                // facky multiplies by itself exponentially
                 facky = facky * size;
-                size = size - 1;
+                size--;
             }
             clunk(facky);
         }
@@ -26,28 +29,200 @@ function thingAMaJig() {
     
     function display(output) {
         console.log(output);
-        clunkCounter = clunkCounter + 1;
+        clunkCounter++;
     }
+    // "clank"
+    // 1
     var clunkCounter = 0;
     thingamajig(0);
     console.log(clunkCounter);
     
+    // "thunk"
+    // 1
     clunkCounter = 0;
     thingamajig(1);
     console.log(clunkCounter);
     
+    // "clunk"
+    // "clunk"
+    // 2 * 1 = "2"
     clunkCounter = 0;
     thingamajig(2);
     console.log(clunkCounter);
     
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // 3 * 2 * 1 = "6"
     clunkCounter = 0;
     thingamajig(3);
     console.log(clunkCounter);
     
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // 4 * 3 * 2 * 1 = "24"
     clunkCounter = 0;
     thingamajig(4);
     console.log(clunkCounter);
     
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // "clunk"
+    // 5 * 4 * 3 * 2 * 1 = "120"
     clunkCounter = 0;
     thingamajig(5);
     console.log(clunkCounter);
