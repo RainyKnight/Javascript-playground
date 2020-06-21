1d55525bec5c4811c8873de4e7855aba3a5a3f67
RainyKnight <stoltmanlucas@gmail.com>
2020-06-21 00:38:58 -0700

    Further explained the flaws in sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -6,6 +6,7 @@ function sherlock() {
 
     // the real balance was never actually stolen because the global variable 
     // is shadowed by a local parameter with the same name...
+    // to fix this, they should have changed the parameter "balance" into literally anything else
     function steal(balance, amount) {
         cameraOn = false;
 
@@ -15,6 +16,7 @@ function sherlock() {
             console.log("Shadowed local balance AFTER: $" + balance);
         }
         return amount;
+        // on top of that, functions stop after a return. So the camera never comes back on...
         cameraOn = true;
     }
 
@@ -25,5 +27,5 @@ function sherlock() {
     alert("Criminal: you stole " + amount + "!");
     alert("Except they didn't really ;)");
 }
-// Of course none of these values are ACTUALLY global to this playground, 
-// because I don't want them messing up any future additions!
+// Of course none of these values are ACTUALLY global to this playground
+// because I don't want them interacting with other js files I might add in the future.
